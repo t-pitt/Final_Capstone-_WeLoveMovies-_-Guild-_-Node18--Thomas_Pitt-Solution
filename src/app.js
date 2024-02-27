1bdc8f20d0cdcccb7deb99d5484925f64b6bb708
Thomas Pitt <tpitt@indeed.com>
2024-02-27 09:41:30 -0800

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
 const app = express();
-// const cors = require("cors")
+const cors = require("cors")
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
